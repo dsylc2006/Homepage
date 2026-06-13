c0c19dceaf9a0e1a15057c8400678c25e4ce3e2e
dsylc2006 <dsylc2006@gmail.com>
2026-06-13 20:08:06 +0800

    Update homepage content

--- data/profile.js
+++ data/profile.js
@@ -21,6 +21,12 @@ window.PROFILE = {
       tags: ["IEEE TETCI", "Accepted"],
       title:
         "论文 “FedSTAFN: A Secure and Efficient Privacy-Preserving Federated Learning Model for Electricity Theft Detection” 已被 IEEE Transactions on Emerging Topics in Computational Intelligence 接收。"
+
+   period: "2026.09",
+      tags: ["IEEE TIP", "Accepted"],
+      title:
+        "论文 “FedSTAFN: A Secure and Efficient Privacy-Preserving Federated Learning Model for Electricity Theft Detection” 已被 IEEE Transactions on Emerging Topics in Computational Intelligence 接收。"
+ 
     }
   ],
   metrics: [
